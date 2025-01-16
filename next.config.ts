dae89fa0e919096bd784f572fd244e3154a5d44b
Andrew <andrew7810262000@gmail.com>
2025-01-16 12:19:25 +0800

    add github-action

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
 };
 
 export default nextConfig;
