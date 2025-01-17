858721300e20a2e0af2ee48c01562135c49c3c5e
Andrew <andrew7810262000@gmail.com>
2025-01-17 10:30:07 +0800

    add demo image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ZH-TW">
+    <html lang="ZH-TW" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,64 @@
+import feature from '@/assets/feature.png';
+import heroSection from '@/assets/hero-section.png';
+import hasGFarm from '@/assets/has-g-farm.png';
+
+// @ts-nocheck
 export default function Home() {
+
+  const info = [
+    {
+      title: 'Hero-section-marquee',
+      codePenId: 'NPKMwmY',
+      image: heroSection,
+    },
+    {
+      title: 'horizontal-parallax',
+      codePenId: 'qEWYVeO',
+      image: hasGFarm,
+    },
+    {
+      title: 'Feature C',
+      codePenId: 'RNbJwVN',
+      image: feature,
+    },
+    {
+      title: 'Feature D - scroll',
+      codePenId: 'dPbeZrG',
+      image: feature,
+    },
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Main
+    <div className="max-h-screen flex flex-col">
+      <main
+        className="snap-mandatory snap-y overflow-y-auto flex-1 w-full flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+        {
+          info.map((item, index) => (
+            <>
+              <div key={`img-${index}`} className="snap-start w-full flex min-h-screen max-h-screen justify-center items-center">
+                <img className="m-auto" style={{maxHeight:'80vh'}} src={item.image.src} />
+              </div>
+              <div key={`codepen-${index}`} className="w-[85vw] gap-2 snap-start min-h-screen flex flex-col justify-center mx-auto">
+                <h4 className="text-3xl font-bold">{item.title}</h4>
+                <iframe height="500"
+                        style={{ width: '100%' }}
+                        scrolling="no"
+                        title={item.title}
+                        src={`https://codepen.io/andrew781026/embed/${item.codePenId}?default-tab=result&editable=true`}
+                        frameBorder="no"
+                        loading="lazy">
+                  See the Pen <a href={`https://codepen.io/andrew781026/pen/${item.codePenId}`}>
+                  {item.title}</a> by 王澍 (<a href="https://codepen.io/andrew781026">@andrew781026</a>)
+                  on <a href="https://codepen.io">CodePen</a>.
+                </iframe>
+              </div>
+            </>
+          ))
+        }
+
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        Footer
-      </footer>
     </div>
   );
 }
